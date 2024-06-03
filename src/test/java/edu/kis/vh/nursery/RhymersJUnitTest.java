0d49b3863151aa067e5f68a57873f1a8303440e2
Maciek Mirowski <m.miro2002@gmail.com>
2024-06-03 09:12:39 +0200

    15. Walidacja projektu testami jednostkowymi. Każdy z nich przeszedł.

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 public class RhymersJUnitTest {
 
+        // Every test passed
     @Test
     public void testCountIn() {
         DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
